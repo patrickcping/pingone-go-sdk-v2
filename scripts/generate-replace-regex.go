2d889815927e8bf925b68838a2a4ff9385664f85
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-04-19 09:35:37 +0100

    Automate autogen manual interventions (1) (#148)
    * automate password policy model intervention
    
    * `model_certificate.go`

--- scripts/generate-replace-regex.go
+++ scripts/generate-replace-regex.go
@@ -14,33 +14,30 @@ func main() {
 	}
 	dir := os.Args[1]
 
-	// Define the target extension
-	ext := ".go"
-
-	// Get a list of all files with the given extension in the directory
-	files, err := filepath.Glob(filepath.Join(dir, "*"+ext))
-	if err != nil {
-		panic(err)
-	}
-
-	// Iterate over the files and apply the regex replacement rules
-	for _, file := range files {
-		// Read the file contents
-		content, err := os.ReadFile(filepath.Clean(file))
+	for _, rule := range replaceRules {
+		// Get a list of all files with the given extension in the directory
+		files, err := filepath.Glob(filepath.Join(dir, rule.fileSelectPattern))
 		if err != nil {
 			panic(err)
 		}
 
-		// Apply the regex replacement rules
-		for _, rule := range replaceRules {
+		// Iterate over the files and apply the regex replacement rules
+		for _, file := range files {
+			// Read the file contents
+			content, err := os.ReadFile(filepath.Clean(file))
+			if err != nil {
+				panic(err)
+			}
+
+			// Apply the regex replacement rule
 			re := regexp.MustCompile(rule.pattern)
 			content = re.ReplaceAll(content, []byte(rule.repl))
-		}
 
-		// Write the updated file contents
-		err = os.WriteFile(file, content, os.ModePerm)
-		if err != nil {
-			panic(err)
+			// Write the updated file contents
+			err = os.WriteFile(file, content, os.ModePerm)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 }
@@ -48,12 +45,15 @@ func main() {
 var (
 	// Define the full list of regex replacement rules
 	replaceRules = []struct {
-		pattern string
-		repl    string
+		fileSelectPattern string
+		pattern           string
+		repl              string
 	}{
+		// Add retryability to typed output
 		{
-			`func \(([a-zA-Z\* ]+)\) ([a-zA-Z])([a-zA-Z]+Execute)\(([a-zA-Z ]*)\) \(([\*a-zA-Z]*), \*http\.Response, error\) {`,
-			`func ($1) $2$3($4) ($5, *http.Response, error) {
+			fileSelectPattern: "api_*.go",
+			pattern:           `func \(([a-zA-Z\* ]+)\) ([a-zA-Z])([a-zA-Z]+Execute)\(([a-zA-Z ]*)\) \(([\*a-zA-Z]*), \*http\.Response, error\) {`,
+			repl: `func ($1) $2$3($4) ($5, *http.Response, error) {
 	obj, response, error := processResponse(
 		func() (interface{}, *http.Response, error) {
 			return r.ApiService.internal$2$3(r)
@@ -64,9 +64,12 @@ var (
 			
 func ($1) internal$2$3($4) ($5, *http.Response, error) {`,
 		},
+
+		// Add retryability to non-typed outputs
 		{
-			`func \(([a-zA-Z\* ]+)\) ([a-zA-Z])([a-zA-Z]+Execute)\(([a-zA-Z ]*)\) \(\*http\.Response, error\) {`,
-			`func ($1) $2$3($4) (*http.Response, error) {
+			fileSelectPattern: "api_*.go",
+			pattern:           `func \(([a-zA-Z\* ]+)\) ([a-zA-Z])([a-zA-Z]+Execute)\(([a-zA-Z ]*)\) \(\*http\.Response, error\) {`,
+			repl: `func ($1) $2$3($4) (*http.Response, error) {
 	_, response, error := processResponse(
 		func() (interface{}, *http.Response, error) {
 			resp, err := r.ApiService.internal$2$3(r)
@@ -77,6 +80,44 @@ func ($1) internal$2$3($4) ($5, *http.Response, error) {`,
 }
 			
 func ($1) internal$2$3($4) (*http.Response, error) {`,
+		},
+
+		// Handle errors for linters
+		// {
+		// 	`	localVarHTTPResponse.Body.Close()`,
+		// 	`	_ = localVarHTTPResponse.Body.Close()`,
+		// },
+
+		// Password policy model
+		{
+			fileSelectPattern: "model_password_policy_min_characters.go",
+			pattern:           `______`,
+			repl:              `SpecialChar`,
+		},
+
+		{
+			fileSelectPattern: "model_password_policy_min_characters.go",
+			pattern:           `json:"~!@\#\$%\^&amp;\*\(\)-_&\#x3D;\+\[]\{}\|;:,\.&lt;&gt;/\?,omitempty"`,
+			repl:              `json:"specialchar,omitempty"`,
+		},
+
+		{
+			fileSelectPattern: "model_password_policy_min_characters.go",
+			pattern:           `toSerialize\["~!@\#\$%\^&amp;\*\(\)-_&\#x3D;\+\[]\{}\|;:,\.&lt;&gt;/\?"]\ =\ o\.SpecialChar`,
+			repl:              `toSerialize["~!@#$%^&*()-_=+[]{}|;:,.<>/?"] = o.SpecialChar`,
+		},
+
+		// Certificate model
+		{
+			fileSelectPattern: "model_certificate.go",
+			pattern:           `int64`,
+			repl:              `big.Int`,
+		},
+		{
+			fileSelectPattern: "model_certificate.go",
+			pattern:           `import \(`,
+			repl: `import (
+	"math/big"`,
 		},
 	}
 )
