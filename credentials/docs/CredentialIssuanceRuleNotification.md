4575f566ed60ae1d2ee6bf7a1f9b59de166c88c4
mjspi <mikesimon@pingidentity.com>
2023-05-08 17:07:25 -0500

    Corrected notification schema in credential issuance api specification.

--- credentials/docs/CredentialIssuanceRuleNotification.md
+++ credentials/docs/CredentialIssuanceRuleNotification.md
@@ -5,6 +5,9 @@
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **Methods** | Pointer to [**[]EnumCredentialIssuanceRuleNotificationMethod**](EnumCredentialIssuanceRuleNotificationMethod.md) |  | [optional] 
+**Locale** | Pointer to **string** | A string that specifies the ISO 2-character language code used for the notification; for example, en. | [optional] 
+**Variables** | Pointer to **map[string]interface{}** | An object of key:value pairs that defines the dynamic variables used by the content variant. | [optional] 
+**Variant** | Pointer to **string** | A string that specifies the unique user-defined name for the content variant that contains the message text used for the notification | [optional] 
 
 ## Methods
 
@@ -49,6 +52,81 @@ SetMethods sets Methods field to given value.
 `func (o *CredentialIssuanceRuleNotification) HasMethods() bool`
 
 HasMethods returns a boolean if a field has been set.
+
+### GetLocale
+
+`func (o *CredentialIssuanceRuleNotification) GetLocale() string`
+
+GetLocale returns the Locale field if non-nil, zero value otherwise.
+
+### GetLocaleOk
+
+`func (o *CredentialIssuanceRuleNotification) GetLocaleOk() (*string, bool)`
+
+GetLocaleOk returns a tuple with the Locale field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetLocale
+
+`func (o *CredentialIssuanceRuleNotification) SetLocale(v string)`
+
+SetLocale sets Locale field to given value.
+
+### HasLocale
+
+`func (o *CredentialIssuanceRuleNotification) HasLocale() bool`
+
+HasLocale returns a boolean if a field has been set.
+
+### GetVariables
+
+`func (o *CredentialIssuanceRuleNotification) GetVariables() map[string]interface{}`
+
+GetVariables returns the Variables field if non-nil, zero value otherwise.
+
+### GetVariablesOk
+
+`func (o *CredentialIssuanceRuleNotification) GetVariablesOk() (*map[string]interface{}, bool)`
+
+GetVariablesOk returns a tuple with the Variables field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetVariables
+
+`func (o *CredentialIssuanceRuleNotification) SetVariables(v map[string]interface{})`
+
+SetVariables sets Variables field to given value.
+
+### HasVariables
+
+`func (o *CredentialIssuanceRuleNotification) HasVariables() bool`
+
+HasVariables returns a boolean if a field has been set.
+
+### GetVariant
+
+`func (o *CredentialIssuanceRuleNotification) GetVariant() string`
+
+GetVariant returns the Variant field if non-nil, zero value otherwise.
+
+### GetVariantOk
+
+`func (o *CredentialIssuanceRuleNotification) GetVariantOk() (*string, bool)`
+
+GetVariantOk returns a tuple with the Variant field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetVariant
+
+`func (o *CredentialIssuanceRuleNotification) SetVariant(v string)`
+
+SetVariant sets Variant field to given value.
+
+### HasVariant
+
+`func (o *CredentialIssuanceRuleNotification) HasVariant() bool`
+
+HasVariant returns a boolean if a field has been set.
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- credentials/model_credential_issuance_rule_notification.go
+++ credentials/model_credential_issuance_rule_notification.go
@@ -20,6 +20,12 @@ var _ MappedNullable = &CredentialIssuanceRuleNotification{}
 // CredentialIssuanceRuleNotification struct for CredentialIssuanceRuleNotification
 type CredentialIssuanceRuleNotification struct {
 	Methods []EnumCredentialIssuanceRuleNotificationMethod `json:"methods,omitempty"`
+	// A string that specifies the ISO 2-character language code used for the notification; for example, en.
+	Locale *string `json:"locale,omitempty"`
+	// An object of key:value pairs that defines the dynamic variables used by the content variant.
+	Variables map[string]interface{} `json:"variables,omitempty"`
+	// A string that specifies the unique user-defined name for the content variant that contains the message text used for the notification
+	Variant *string `json:"variant,omitempty"`
 }
 
 // NewCredentialIssuanceRuleNotification instantiates a new CredentialIssuanceRuleNotification object
@@ -71,6 +77,102 @@ func (o *CredentialIssuanceRuleNotification) SetMethods(v []EnumCredentialIssuan
 	o.Methods = v
 }
 
+// GetLocale returns the Locale field value if set, zero value otherwise.
+func (o *CredentialIssuanceRuleNotification) GetLocale() string {
+	if o == nil || IsNil(o.Locale) {
+		var ret string
+		return ret
+	}
+	return *o.Locale
+}
+
+// GetLocaleOk returns a tuple with the Locale field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *CredentialIssuanceRuleNotification) GetLocaleOk() (*string, bool) {
+	if o == nil || IsNil(o.Locale) {
+		return nil, false
+	}
+	return o.Locale, true
+}
+
+// HasLocale returns a boolean if a field has been set.
+func (o *CredentialIssuanceRuleNotification) HasLocale() bool {
+	if o != nil && !IsNil(o.Locale) {
+		return true
+	}
+
+	return false
+}
+
+// SetLocale gets a reference to the given string and assigns it to the Locale field.
+func (o *CredentialIssuanceRuleNotification) SetLocale(v string) {
+	o.Locale = &v
+}
+
+// GetVariables returns the Variables field value if set, zero value otherwise.
+func (o *CredentialIssuanceRuleNotification) GetVariables() map[string]interface{} {
+	if o == nil || IsNil(o.Variables) {
+		var ret map[string]interface{}
+		return ret
+	}
+	return o.Variables
+}
+
+// GetVariablesOk returns a tuple with the Variables field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *CredentialIssuanceRuleNotification) GetVariablesOk() (map[string]interface{}, bool) {
+	if o == nil || IsNil(o.Variables) {
+		return map[string]interface{}{}, false
+	}
+	return o.Variables, true
+}
+
+// HasVariables returns a boolean if a field has been set.
+func (o *CredentialIssuanceRuleNotification) HasVariables() bool {
+	if o != nil && !IsNil(o.Variables) {
+		return true
+	}
+
+	return false
+}
+
+// SetVariables gets a reference to the given map[string]interface{} and assigns it to the Variables field.
+func (o *CredentialIssuanceRuleNotification) SetVariables(v map[string]interface{}) {
+	o.Variables = v
+}
+
+// GetVariant returns the Variant field value if set, zero value otherwise.
+func (o *CredentialIssuanceRuleNotification) GetVariant() string {
+	if o == nil || IsNil(o.Variant) {
+		var ret string
+		return ret
+	}
+	return *o.Variant
+}
+
+// GetVariantOk returns a tuple with the Variant field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *CredentialIssuanceRuleNotification) GetVariantOk() (*string, bool) {
+	if o == nil || IsNil(o.Variant) {
+		return nil, false
+	}
+	return o.Variant, true
+}
+
+// HasVariant returns a boolean if a field has been set.
+func (o *CredentialIssuanceRuleNotification) HasVariant() bool {
+	if o != nil && !IsNil(o.Variant) {
+		return true
+	}
+
+	return false
+}
+
+// SetVariant gets a reference to the given string and assigns it to the Variant field.
+func (o *CredentialIssuanceRuleNotification) SetVariant(v string) {
+	o.Variant = &v
+}
+
 func (o CredentialIssuanceRuleNotification) MarshalJSON() ([]byte, error) {
 	toSerialize,err := o.ToMap()
 	if err != nil {
@@ -83,6 +185,15 @@ func (o CredentialIssuanceRuleNotification) ToMap() (map[string]interface{}, err
 	toSerialize := map[string]interface{}{}
 	if !IsNil(o.Methods) {
 		toSerialize["methods"] = o.Methods
+	}
+	if !IsNil(o.Locale) {
+		toSerialize["locale"] = o.Locale
+	}
+	if !IsNil(o.Variables) {
+		toSerialize["variables"] = o.Variables
+	}
+	if !IsNil(o.Variant) {
+		toSerialize["variant"] = o.Variant
 	}
 	return toSerialize, nil
 }
