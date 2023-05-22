4026b0e51798f68515f5bbfe40e5a9e51739c414
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2023-05-22 09:11:49 +0100

    Added `filterOptions.ipAddressExposed` and `filterOptions.userAgentExposed` to the `Subscription` (webhook) data model. (#173)
    * Added `filterOptions.ipAddressExposed` and `filterOptions.userAgentExposed` to the `Subscription` (webhook) data model.
    
    * changelog

--- management/configuration.go
+++ management/configuration.go
@@ -89,7 +89,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "PingOne-GOLANG-SDK/0.20.1/go",
+		UserAgent:        "PingOne-GOLANG-SDK/0.21.0/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
