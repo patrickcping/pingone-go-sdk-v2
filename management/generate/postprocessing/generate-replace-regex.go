54fb2510b2427511316961d63e0634aff7b69d01
M. Simon <100732743+mjspi@users.noreply.github.com>
2023-10-30 06:52:36 -0500

    `EntityArrayEmbeddedGatewaysInner` correction to properly handle RADIUS Gateway Types (#273)
    * Corrected situations where `EntityArrayEmbeddedGatewaysInner` unmarshal did not return ENUMGATEWAYTYPE_RADIUS objects properly.
    
    * Changelog update.
    
    * Adjustments now `EntityArrayEmbeddedGatewaysInner` model is added to the postprocessor
    
    * changelogs
    
    ---------
    
    Co-authored-by: Patrick Cowland <patrickcowland@pingidentity.com>

--- management/generate/postprocessing/generate-replace-regex.go
+++ management/generate/postprocessing/generate-replace-regex.go
@@ -242,5 +242,70 @@ var (
 
 // Marshal data from the first non-nil pointers in the struct to JSON`,
 		},
+
+		// EntityArrayEmbeddedGatewaysInner model
+		{
+			fileSelectPattern: "model_entity_array__embedded_gateways_inner.go",
+			pattern:           `(func \(dst \*EntityArrayEmbeddedGatewaysInner\) UnmarshalJSON\(data \[\]byte\) error \{\n)((.*)\n)*\}\n\n\/\/ Marshal data from the first non-nil pointers in the struct to JSON`,
+			repl: `func (dst *EntityArrayEmbeddedGatewaysInner) UnmarshalJSON(data []byte) error {
+
+	var err error
+	// try to unmarshal JSON data into Gateway
+	err = json.Unmarshal(data, &dst.Gateway)
+	if err == nil {
+		jsonGateway, _ := json.Marshal(dst.Gateway)
+		if string(jsonGateway) == "{}" { // empty struct
+			dst.Gateway = nil
+		} else {
+			switch dst.Gateway.Type {
+			case ENUMGATEWAYTYPE_LDAP, ENUMGATEWAYTYPE_RADIUS:
+				dst.Gateway = nil
+			default:
+				return nil // data stored in dst.Gateway, return on the first match
+			}
+		}
+	} else {
+		dst.Gateway = nil
+	}
+
+	// try to unmarshal JSON data into GatewayTypeLDAP
+	err = json.Unmarshal(data, &dst.GatewayTypeLDAP)
+	if err == nil {
+		jsonGatewayLDAP, _ := json.Marshal(dst.GatewayTypeLDAP)
+		if string(jsonGatewayLDAP) == "{}" { // empty struct
+			dst.GatewayTypeLDAP = nil
+		} else {
+			if dst.GatewayTypeLDAP.Type == ENUMGATEWAYTYPE_LDAP {
+				return nil // data stored in dst.GatewayLDAP, return on the first match
+			} else {
+				dst.GatewayTypeLDAP = nil
+			}
+		}
+	} else {
+		dst.GatewayTypeLDAP = nil
+	}
+
+	// try to unmarshal JSON data into GatewayTypeRADIUS
+	err = json.Unmarshal(data, &dst.GatewayTypeRADIUS)
+	if err == nil {
+		jsonGatewayRADIUS, _ := json.Marshal(dst.GatewayTypeRADIUS)
+		if string(jsonGatewayRADIUS) == "{}" { // empty struct
+			dst.GatewayTypeRADIUS = nil
+		} else {
+			if dst.GatewayTypeRADIUS.Type == ENUMGATEWAYTYPE_RADIUS {
+				return nil // data stored in dst.GatewayTypeRADIUS, return on the first match
+			} else {
+				dst.GatewayTypeRADIUS = nil
+			}
+		}
+	} else {
+		dst.GatewayTypeRADIUS = nil
+	}
+
+	return fmt.Errorf("Data failed to match schemas in anyOf(EntityArrayEmbeddedGatewaysInner)")
+}
+
+// Marshal data from the first non-nil pointers in the struct to JSON`,
+		},
 	}
 )
