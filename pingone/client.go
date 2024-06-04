3f3ce2175e72e4ba47b67468b6876796dfbfa587
Patrick Cowland <patrickcowland@pingidentity.com>
2024-06-04 13:50:11 +0100

    version uplift

--- pingone/client.go
+++ pingone/client.go
@@ -30,7 +30,7 @@ type Client struct {
 	Region               model.RegionMapping
 }
 
-var version = "0.11.9"
+var version = "0.12.0"
 
 func (c *Config) APIClient(ctx context.Context) (*Client, error) {
 
