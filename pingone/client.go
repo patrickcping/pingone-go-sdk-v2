a525b61e3779acf94f95de9c36ac642891c3b35f
Patrick Cowland <patrickcowland@pingidentity.com>
2024-07-04 09:27:28 +0100

    bump version

--- pingone/client.go
+++ pingone/client.go
@@ -30,7 +30,7 @@ type Client struct {
 	Region               model.RegionMapping
 }
 
-var version = "0.12.0"
+var version = "0.12.2"
 
 func (c *Config) APIClient(ctx context.Context) (*Client, error) {
 
