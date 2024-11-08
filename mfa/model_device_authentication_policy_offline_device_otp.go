14faca50d66dadd3b0390aa47a2a0e0a0c93f1b6
Patrick Cowland <patrickcowland@pingidentity.com>
2024-11-08 15:28:13 +0000

    Added the `OtpLength` field to the `DeviceAuthenticationPolicyOfflineDeviceOtp` data model

--- mfa/model_device_authentication_policy_offline_device_otp.go
+++ mfa/model_device_authentication_policy_offline_device_otp.go
@@ -21,6 +21,8 @@ var _ MappedNullable = &DeviceAuthenticationPolicyOfflineDeviceOtp{}
 type DeviceAuthenticationPolicyOfflineDeviceOtp struct {
 	LifeTime DeviceAuthenticationPolicyOfflineDeviceOtpLifeTime `json:"lifeTime"`
 	Failure DeviceAuthenticationPolicyOfflineDeviceOtpFailure `json:"failure"`
+	// Used to specify the length of the OTP that is shown to users. Minimum length is `6` digits and maximum is `10` digits. If the parameter is not provided, the default is `6` digits.
+	OtpLength *int32 `json:"otpLength,omitempty"`
 }
 
 // NewDeviceAuthenticationPolicyOfflineDeviceOtp instantiates a new DeviceAuthenticationPolicyOfflineDeviceOtp object
@@ -31,6 +33,8 @@ func NewDeviceAuthenticationPolicyOfflineDeviceOtp(lifeTime DeviceAuthentication
 	this := DeviceAuthenticationPolicyOfflineDeviceOtp{}
 	this.LifeTime = lifeTime
 	this.Failure = failure
+	var otpLength int32 = 6
+	this.OtpLength = &otpLength
 	return &this
 }
 
@@ -39,6 +43,8 @@ func NewDeviceAuthenticationPolicyOfflineDeviceOtp(lifeTime DeviceAuthentication
 // but it doesn't guarantee that properties required by API are set
 func NewDeviceAuthenticationPolicyOfflineDeviceOtpWithDefaults() *DeviceAuthenticationPolicyOfflineDeviceOtp {
 	this := DeviceAuthenticationPolicyOfflineDeviceOtp{}
+	var otpLength int32 = 6
+	this.OtpLength = &otpLength
 	return &this
 }
 
@@ -90,6 +96,38 @@ func (o *DeviceAuthenticationPolicyOfflineDeviceOtp) SetFailure(v DeviceAuthenti
 	o.Failure = v
 }
 
+// GetOtpLength returns the OtpLength field value if set, zero value otherwise.
+func (o *DeviceAuthenticationPolicyOfflineDeviceOtp) GetOtpLength() int32 {
+	if o == nil || IsNil(o.OtpLength) {
+		var ret int32
+		return ret
+	}
+	return *o.OtpLength
+}
+
+// GetOtpLengthOk returns a tuple with the OtpLength field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *DeviceAuthenticationPolicyOfflineDeviceOtp) GetOtpLengthOk() (*int32, bool) {
+	if o == nil || IsNil(o.OtpLength) {
+		return nil, false
+	}
+	return o.OtpLength, true
+}
+
+// HasOtpLength returns a boolean if a field has been set.
+func (o *DeviceAuthenticationPolicyOfflineDeviceOtp) HasOtpLength() bool {
+	if o != nil && !IsNil(o.OtpLength) {
+		return true
+	}
+
+	return false
+}
+
+// SetOtpLength gets a reference to the given int32 and assigns it to the OtpLength field.
+func (o *DeviceAuthenticationPolicyOfflineDeviceOtp) SetOtpLength(v int32) {
+	o.OtpLength = &v
+}
+
 func (o DeviceAuthenticationPolicyOfflineDeviceOtp) MarshalJSON() ([]byte, error) {
 	toSerialize,err := o.ToMap()
 	if err != nil {
@@ -102,6 +140,9 @@ func (o DeviceAuthenticationPolicyOfflineDeviceOtp) ToMap() (map[string]interfac
 	toSerialize := map[string]interface{}{}
 	toSerialize["lifeTime"] = o.LifeTime
 	toSerialize["failure"] = o.Failure
+	if !IsNil(o.OtpLength) {
+		toSerialize["otpLength"] = o.OtpLength
+	}
 	return toSerialize, nil
 }
 
