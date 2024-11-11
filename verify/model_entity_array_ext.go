213dfcfd9fe4f897d48af1eae5b355325bf078f3
Patrick Cowland <patrickcowland@pingidentity.com>
2024-11-11 15:11:22 +0000

    add further `EntityArray` pagination helpers

--- verify/model_entity_array_ext.go
+++ verify/model_entity_array_ext.go
@@ -18,6 +18,29 @@ func (o *EntityArray) hasHalLink(linkIndex string) bool {
 	return false
 }
 
+func (o *EntityArray) getHalLink(linkIndex string) LinksHATEOASValue {
+	if l, ok := o.GetLinksOk(); ok && l != nil {
+		links := *l
+		if v, ok := links[linkIndex]; ok {
+			return v
+		}
+	}
+
+	var ret LinksHATEOASValue
+	return ret
+}
+
+func (o *EntityArray) getHalLinkOk(linkIndex string) (*LinksHATEOASValue, bool) {
+	if l, ok := o.GetLinksOk(); ok && l != nil {
+		links := *l
+		if v, ok := links[linkIndex]; ok {
+			return &v, true
+		}
+	}
+
+	return nil, false
+}
+
 func (o *EntityArray) IsPaginated() bool {
 	return o.hasHalLink(PAGINATION_HAL_LINK_INDEX_NEXT) || o.hasHalLink(PAGINATION_HAL_LINK_INDEX_PREV)
 }
@@ -26,10 +49,34 @@ func (o *EntityArray) HasPaginationSelf() bool {
 	return o.hasHalLink(PAGINATION_HAL_LINK_INDEX_SELF)
 }
 
+func (o *EntityArray) GetPaginationSelfLink() LinksHATEOASValue {
+	return o.getHalLink(PAGINATION_HAL_LINK_INDEX_SELF)
+}
+
+func (o *EntityArray) GetPaginationSelfLinkOk() (*LinksHATEOASValue, bool) {
+	return o.getHalLinkOk(PAGINATION_HAL_LINK_INDEX_SELF)
+}
+
 func (o *EntityArray) HasPaginationNext() bool {
 	return o.hasHalLink(PAGINATION_HAL_LINK_INDEX_NEXT)
 }
 
+func (o *EntityArray) GetPaginationNextLink() LinksHATEOASValue {
+	return o.getHalLink(PAGINATION_HAL_LINK_INDEX_NEXT)
+}
+
+func (o *EntityArray) GetPaginationNextLinkOk() (*LinksHATEOASValue, bool) {
+	return o.getHalLinkOk(PAGINATION_HAL_LINK_INDEX_NEXT)
+}
+
 func (o *EntityArray) HasPaginationPrevious() bool {
 	return o.hasHalLink(PAGINATION_HAL_LINK_INDEX_PREV)
+}
+
+func (o *EntityArray) GetPaginationPreviousLink() LinksHATEOASValue {
+	return o.getHalLink(PAGINATION_HAL_LINK_INDEX_PREV)
+}
+
+func (o *EntityArray) GetPaginationPreviousLinkOk() (*LinksHATEOASValue, bool) {
+	return o.getHalLinkOk(PAGINATION_HAL_LINK_INDEX_PREV)
 }
