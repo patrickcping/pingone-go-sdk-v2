07bafb8083e2704c32f1d845197196f83d39f4e5
Patrick Cowland <patrickcowland@pingidentity.com>
2024-11-20 15:45:13 +0000

    remove client_ext

--- scripts/generate-module.sh
+++ scripts/generate-module.sh
@@ -14,11 +14,6 @@ else
         # openapi-generator-cli version-manager set 7.0.1
         openapi-generator-cli generate -i ../.generate/pingone-$3.yml -g go --additional-properties=packageName=$3,packageVersion=$version,isGoSubmodule=true,enumClassPrefix=true,apiNameSuffix=Api -o . --git-repo-id $2 --git-user-id $1 --http-user-agent \"pingtools PingOne-GOLANG-SDK-$3/$version\" -t ../.generate/templates; \
 
-        echo "==> Copying custom templated files..."
-        template=$(cat ../scripts/templates/client_ext.go.tmpl)
-        template=${template//PACKAGENAME/$3}
-        echo "$template" > "client_ext.go"
-
         template=$(cat ../scripts/templates/model_entity_array_ext.go.tmpl)
         template=${template//PACKAGENAME/$3}
         echo "$template" > "model_entity_array_ext.go"
