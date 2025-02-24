32079cd40cb1decb58fddf6861612b229a7046c2
Patrick Cowland <patrickcowland@pingidentity.com>
2025-02-24 10:37:07 +0000

    bump version for next release

--- GNUmakefile
+++ GNUmakefile
@@ -2,7 +2,7 @@
 TEST?=$$(go list ./...)
 OWNER=patrickcping
 REPO=pingone-go-sdk-v2
-VERSION=0.12.10
+VERSION=0.12.11
 
 default: build
 
--- pingone/client.go
+++ pingone/client.go
@@ -30,7 +30,7 @@ type Client struct {
 	Region               model.RegionMapping
 }
 
-var version = "0.12.10"
+var version = "0.12.11"
 
 func (c *Config) APIClient(ctx context.Context) (*Client, error) {
 
