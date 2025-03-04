18f273887d6e92060b398c804fd0a426e9159fae
Patrick Cowland <44225864+patrickcping@users.noreply.github.com>
2025-03-04 10:02:48 +0000

    bump go modules 4-Mar-2025 (#432)
    * bump go modules 4-Mar-2025
    
    * changelog

--- pingone/client.go
+++ pingone/client.go
@@ -30,7 +30,7 @@ type Client struct {
 	Region               model.RegionMapping
 }
 
-var version = "0.12.11"
+var version = "0.12.12"
 
 func (c *Config) APIClient(ctx context.Context) (*Client, error) {
 
