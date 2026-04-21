c088dd8f85cd534ce60d407e52c468807eeededb
Henry Recker <henryrecker@pingidentity.com>
2026-04-21 11:19:18 -0500

    Code formatting

--- verify/model_links_hateoas_next.go
+++ verify/model_links_hateoas_next.go
@@ -73,7 +73,7 @@ func (o *LinksHATEOASNext) SetHref(v string) {
 }
 
 func (o LinksHATEOASNext) MarshalJSON() ([]byte, error) {
-	toSerialize,err := o.ToMap()
+	toSerialize, err := o.ToMap()
 	if err != nil {
 		return []byte{}, err
 	}
@@ -123,5 +123,3 @@ func (v *NullableLinksHATEOASNext) UnmarshalJSON(src []byte) error {
 	v.isSet = true
 	return json.Unmarshal(src, &v.value)
 }
-
-
